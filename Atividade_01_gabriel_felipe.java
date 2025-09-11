9659560ed60603f299de0538e21328d7264458db
Phelipinho001 <gabeielfelipe892@gmail.com>
2025-09-11 20:34:55 -0300

    Add files via upload

---
+++ Atividade_01_gabriel_felipe.java
@@ -0,0 +1,43 @@
+import java.lang.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+  //       String nome;
+		// int idade;
+		// double altura;
+		// boolean estudante;
+		
+		// nome = "Gabriel";
+		// idade = 16;
+		// altura = 1.80; 
+		// estudante = false;
+		
+		// System.out.println(nome);
+		// System.out.println(idade);
+		// System.out.println(altura);
+		// System.out.println(estudante);
+
+
+        int A;
+        int B;
+        int C;
+        double D;
+
+        A = 25;
+        B = A * 3;
+        C = 10;
+        D = A / C;
+
+        System.out.println(A);
+		System.out.println(B);
+		System.out.println(C);
+		System.out.println(D);
+
+
+        
+	
+    }
+}
