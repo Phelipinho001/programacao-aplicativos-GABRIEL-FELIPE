350680c697c6269cbb8f823564b28ed12fdb83f1
Phelipinho001 <gabeielfelipe892@gmail.com>
2025-09-17 21:33:38 -0300

    Add files via upload

---
+++ Atividade03_Gabrielfelipe_Ingridk.java
@@ -0,0 +1,236 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03_gabrielfelipe_ingrid;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade03_Gabrielfelipe_Ingrid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("INgrid viada");
+         Scanner entrada = new Scanner(System.in);
+        
+//        double acucar,sum;
+//        
+//        System.out.print("Digite a entradaa de açucar em gramas ");
+//        acucar = entrada.nextDouble();
+//        
+//        sum = (200*acucar)/8 ;
+//        
+//        System.out.println(sum);
+        
+//        Questão 2
+        
+//        int impressoras, s;
+//        
+//        System.out.print("Quantas impressoras estão trabalhando :");
+//        impressoras = entrada.nextInt();
+//       
+//        s = (impressoras * 12) /3 ;
+//       
+//        System.out.print("Total de minutos foi" + s);
+    
+//      Questão 3
+//
+//        double km,l;
+//        
+//        System.out.print("Digite a quantidade de km da corrida : ");
+//        km = entrada.nextDouble();
+//        
+//        l = (km * 18)/ 240;
+//        
+//        System.out.println("A quantidade de litros é :" + l);
+//        
+//      Questão 4
+
+//        int maquinas;
+//        double horas,prod,pecas;
+//        
+//        System.out.print("Digite a quantidade de maquinas e horas");
+//        maquinas = entrada.nextInt();
+//        horas = entrada.nextDouble();
+//        
+//        prod = 900/(6*5);
+//        
+//        pecas = maquinas * horas * prod;
+//        
+//        System.out.println(pecas);
+        
+//      Questão 5
+
+        
+//        double vusd,vrl,cotacao;
+//        
+//        System.out.print("Digite o valor da cotação do real(de dolar para real) :");
+//        vrl = entrada.nextDouble();
+//        System.out.print("Quantos dolares(USD)você quer comprar ?");
+//        vusd = entrada.nextDouble();
+//        
+//        cotacao = vrl * vusd;
+//
+//       System.out.println("A cotação do real pro dolar é de " + cotacao);
+
+//      Questão 6
+        
+//        Double a,b,cal,x;
+//        System.out.print("Digite o valor de \'a' e 'b\' :");
+//        a = entrada.nextDouble();
+//        b = entrada.nextDouble();
+//        
+//        x = -b/a;
+//        cal = a*x + b ;
+//        
+//        System.out.println("o valor de \'a' , 'b' e 'x\' é ");
+//        System.out.println("a "+a);
+//        System.out.println("b "+b);
+//        System.out.println("x "+x);
+       
+//      Questão 7
+        
+//        double m,b,x0,y0;
+//        
+//        System.out.print("Digite o valor de \'m' , 'b' e 'x0\' : ");
+//        m = entrada.nextDouble();
+//        b = entrada.nextDouble();
+//        x0 = entrada.nextDouble();
+//        
+//        y0 = m * x0 + b;
+//        
+//        System.out.println("O valor de \'y0\' é  " + y0);
+
+//      Questão 8
+
+//        double x2,x1,y1,y2,m,b,y,x;
+//        
+//        System.out.print("Me de o valor de x1 e x2 :");
+//        x1 = entrada.nextDouble();
+//        x2 = entrada.nextDouble();
+//        
+//        System.out.print("Me de o valor de y1 e y2 :");
+//        y1 = entrada.nextDouble();
+//        y2 = entrada.nextDouble();
+//        
+//        m =(y2 - y1)/(x2-x1);
+//        b = y1-m*x1;
+//        y = m*x1+b;
+
+//      Questão 9
+
+//         double m1,b1,m2,b2,x,y;
+//         
+//          System.out.print("Me de o valor de m1 e m2 :");
+//          m1 = entrada.nextDouble();
+//          m2 = entrada.nextDouble();
+//          
+//          System.out.print("Me de o valor de b1 e b2 :");
+//          b1 = entrada.nextDouble();
+//          b2 = entrada.nextDouble();
+//          
+//          x = (b2-b1)/(m2-m1);
+//          y = m1 * x + b1;
+//          
+//          System.out.print("O valor de \'x\' é " + x);
+//          System.out.print("O valor de \'y\' é " + y);
+
+//        Questão 10
+
+//           double a,b,c,del,x1,x2;;
+//           
+//           System.out.print("me de o valor de \'a' , 'b' e 'c\' : ");
+//           a = entrada.nextDouble();
+//           b = entrada.nextDouble();
+//           c = entrada.nextDouble();
+//           
+//           del =  Math.pow(b, 2)-4*a*c;
+//           x1 = (-b)+ Math.sqrt(del)/(2*a);
+//           x2 = (-b)- Math.sqrt(del)/(2*a);
+//           
+//           System.out.println("O valor de delta é " + del);
+//           System.out.println("O valor de x1 é " + x1);
+//           System.out.println("O valor de x2 é " + x2);
+
+//          Questão 11
+
+//        double delta,a,d;
+//        
+//        System.out.print("Me fale o valor do delta e do \'a\' :");
+//        delta = entrada.nextDouble();
+//        a = entrada.nextDouble();
+//        
+//        d = Math.abs((Math.sqrt(delta))/a);
+//        
+//        System.out.println("O valor de \'d\' : ");
+
+//        Questão 12
+
+//            double a,b,c,x0,f,f2;
+//            System.out.print("Me de o valor de \'a' , 'b' e 'c\' : ");
+//            a = entrada.nextDouble();
+//            b = entrada.nextDouble();
+//            c = entrada.nextDouble();
+//            
+//            System.out.print("Me de o valor de \'x0\' : ");
+//            x0 = entrada.nextDouble();
+//            
+//            f = a * (Math.pow(x0, 2)) + b * x0 + c;
+//            f2 = 2 * a * x0 + b;
+//            
+//            System.out.println("O resultado de \'f(x0)\' é: " +f);
+//            System.out.println("O resultado de \'f'(x0)\' é: "+f2);
+            
+//        Questão 13 
+              
+              double m,b,x1,x2,y1,y2,a;
+              
+              System.out.print("Me de os valores de \'m' e 'b\' : ");
+              m = entrada.nextDouble();
+              b = entrada.nextDouble();
+              
+              System.out.print("Me de os valores de \'x1' e 'x2\' : ");
+              x1 = entrada.nextDouble();
+              x2 = entrada.nextDouble();
+              
+              y1 = m * x1 + b; 
+              y2 = m * x2 + b; 
+              
+              a = ((y1 + y2)/2) * (x2-x1); 
+              
+              System.out.println("O resultado da área é: "+a);
+              
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+        
+        // TODO code application logic here
+    }
+    
+
