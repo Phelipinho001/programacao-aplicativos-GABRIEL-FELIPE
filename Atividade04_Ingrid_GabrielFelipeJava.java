9f34e9a59a17b699c31d41f18dfda0f6be82278f
Phelipinho001 <gabeielfelipe892@gmail.com>
2025-09-18 21:37:38 -0300

    Add files via upload

---
+++ Atividade04_Ingrid_GabrielFelipeJava.java
@@ -0,0 +1,307 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade04_ingrid_gabrielfelipe.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04_Ingrid_GabrielFelipeJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Gabriel gay");
+        System.out.println("Ingrid lesbica");
+         Scanner input = new Scanner(System.in);
+         
+//        Questão 1
+        
+//        double num;
+//        System.out.print("Digite um número: ");
+//        num = input.nextDouble();
+//        
+//        if (num < 0){
+//            System.out.println("O número que você digitou é negativo. ");
+//        }else{
+//            System.out.println("O número que você digitou é positivo. ");
+//        }
+//        
+//        Questão 2
+           
+//        double verf;
+//        
+//        System.out.print("Digite um numero : ");
+//        verf = input.nextDouble();
+//        
+//        if (verf < 0){
+//            System.out.println("Você digitou um nº negativo ");
+//        }
+//        else if (verf == 0){
+//            System.out.println("Seu numero é zero");
+//        }   
+//        else {
+//            System.out.println("Seu numero é positivo");
+//        }
+
+//        Questão 3
+
+//        double nota1, nota2, nota3, media;
+//          
+//        System.out.print("Digite as três notas: ");
+//        nota1 = input.nextDouble();
+//        nota2 = input.nextDouble();
+//        nota3 = input.nextDouble();
+//        
+//        media = (nota1 + nota2 + nota3)/3;
+//        System.out.println("A média das notas é: "+media);
+//        
+//        if (media >= 7){
+//            System.out.println("Aluno aprovado! ");
+//        }else{
+//            System.out.println("Aluno reprovado! ");
+//        }
+
+//        Questão 4
+          
+//        double nota1, nota2, nota3, media;
+//          
+//        System.out.print("Digite as três notas: ");
+//        nota1 = input.nextDouble();
+//        nota2 = input.nextDouble();
+//        nota3 = input.nextDouble();
+//        
+//        media = (nota1 + nota2 + nota3)/3;
+//        System.out.println("A média das notas é: "+media);
+//        
+//        if (media >= 7){
+//            System.out.println("Aluno aprovado! ");
+//        }else if(media >= 5 && media < 7){
+//            System.out.println("Aluno em recuperação! ");
+//        }else{
+//            System.out.println("Aluno reprovado! ");
+//        }          
+
+//         Questão 5 
+
+//    double numb;
+//    
+//    System.out.print("Digite um numero : ");
+//    numb = input.nextDouble();
+//    
+//     if (numb % 2==0){
+//        System.out.print("Seu numero é par ");
+//    }
+//     else {
+//         System.out.println("Seu numero é impar ");
+//     }
+        
+//         Questão 6
+
+//    double num1,num2,num3,maior;
+//    
+//    System.out.print("Disque 3 numeros : ");
+//    num1 = input.nextDouble();
+//    num2 = input.nextDouble();
+//    num3 = input.nextDouble();
+//    
+//    maior = Math.max(num1,Math.max (num2, num3));
+//      
+//    System.out.println("Seu numero maior é " + maior );
+
+//      Questão 7
+
+//    int idd;
+//    
+//    System.out.print("Digite a idade do nadador :");
+//    idd = input.nextInt();
+//    
+//    if (idd >= 18 ){
+//        System.out.println("Sua categoria de nadador é Adulto");
+//    }
+//    else if (idd >= 14){
+//        System.out.print("Sua categoria de nadador é Juvenil");
+//    }
+//    else if (idd >= 9){
+//        System.out.print("Sua categoria de nadador è Infantil"); 
+//    }
+//    else if (9 > idd && idd  > 0  ){
+//        System.out.print("Sua categoria é Mirim");
+//    }    
+//    else{
+//        System.out.print("Essa idade ainda n tem categoria ou é invalida");
+//    }
+            
+//      Questão 8
+
+//        double numbs1;
+//        double numbs2,sum;
+//        
+//        System.out.print("Digite doi numeros :");
+//        numbs1 = input.nextDouble();
+//        numbs2 = input.nextDouble();
+//        
+//        sum = numbs1 + numbs2;
+//        
+//        if (sum > 20 ){
+//            sum = sum + 8;
+//            System.out.print("A soma dos seus numeros são" + sum );
+//        }
+//        else {
+//            sum = sum - 5;
+//            System.out.print("A soma dos seus numeros são" + sum);
+//        }
+
+//          Questão 9
+
+//        double produto,aumento,aumento2 ;
+//        
+//        System.out.print("Digite o valor do produto :");
+//        produto = input.nextDouble();
+//        
+//        if (produto < 20){
+//            aumento = produto + (produto * 0.45);
+//            System.out.print("O valor do produto será " + aumento);
+//        }
+//        else {
+//           aumento2 =  produto +(produto * 0.30);
+//           System.out.print("O valor do produto será " + aumento2);
+//            
+//        }
+
+//      Questão 10
+
+//       int macas;
+//       double calc,calc2;
+//        
+//        System.out.print("Quantas maçãs você ira comprar :");
+//        macas = input.nextInt();
+//        
+//        if(macas < 12){
+//            calc = macas * 0.30;
+//            System.out.print("O valor da sua compra é de " + calc);
+//        }
+//        
+//        else{
+//            calc2 = macas * 0.25;
+//            System.out.print("O valor da sua compra é de " + calc2);
+//        }
+
+//      Questão 11
+
+//        double altura,sexo,cal,cal2;
+//        
+//        
+//        System.out.print("Digite sua altura e seu sexo: ");
+//        altura = input.nextDouble();
+//        sexo = input.nextDouble();
+//        
+//        if (sexo == 2){
+//            cal = (72.7 * altura) - 58;
+//            System.out.println("Seu peso masculino ideal, baseado na sua altura, é de: "+cal);
+//        }else if(sexo == 1){
+//            cal2 = (62.1 * altura) - 44.7;
+//            System.out.println("Seu peso feminimo ideal, baseado na sua altura, é de: "+cal2);
+//            }
+//        Questão 12
+//
+//            int dia;
+//            
+//            System.out.print("Fale um dia da semana :");
+//            dia = input.nextInt();
+//                
+//        switch (dia){
+//            case 1 -> System.out.print("Domingo , final de semana");
+//            case 2 -> System.out.print("Dia util");
+//            case 3 -> System.out.print("Dia util");
+//            case 4 -> System.out.print("Dia util");
+//            case 5 -> System.out.print("Dia util");
+//            case 6 -> System.out.print("Dia util");
+//            case 7 -> System.out.print("Sabado ,final de semana");
+//        }
+//         
+
+//          Questão 13
+
+//            int cod_prod;
+//            double soma;
+//            
+//            System.out.print("Digite o código do produto: ");
+//            cod_prod = input.nextInt();
+//            soma = 0.0;
+//            
+//            switch (cod_prod){
+//                case 100:
+//                    System.out.print("Detergente - R$1,59");
+//                    soma = soma + 1.59;
+//                    break;
+//                
+//                case 101:
+//                    System.out.print("Esponja - R$4.59");
+//                    soma = soma + 4.59;
+//                    break;
+//                            
+//                case 102:
+//                    System.out.print("Lã de aço - R$1,79");
+//                    soma = soma + 1.79;
+//                    break;
+//                            
+//                default:
+//                    System.out.print("Código inválido! ");
+//            }
+        
+//            Questão 14
+            
+            
+        System.out.println("Escolha a Operacao :");
+                
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtracao");
+        System.out.println("3 - Multiplicacao");
+        System.out.println("4 - Divisao");
+        
+        System.out.println("Escolha: ");
+        int opcao = input.nextInt(); 
+        
+        switch (opcao) {
+            case 1 ->                 {
+                    System.out.println("prim. Num: ");
+                    double n1 = input.nextDouble();
+                    System.out.println("seg. Num: ");
+                    double n2 = input.nextDouble();
+                    System.out.println("Resultado = "+ (n1 + n2));
+                }
+            case 2 ->                 {
+                    System.out.println("prim. Num: ");
+                    double n1 = input.nextDouble();
+                    System.out.println("seg. Num: ");
+                    double n2 = input.nextDouble();
+                    System.out.println("Resultado = "+ (n1 - n2));
+                }
+            case 3 ->                 {
+                    System.out.println("prim. Num: ");
+                    double n1 = input.nextDouble();
+                    System.out.println("seg. Num: ");
+                    double n2 = input.nextDouble();
+                    System.out.println("Resultado = "+ (n1 * n2));
+                }
+            case 4 ->                 {
+                    System.out.println("prim. Num: ");
+                    double n1 = input.nextDouble();
+                    System.out.println("seg. Num: ");
+                    double n2 = input.nextDouble();
+                    System.out.println("Resultado = "+ (n1 / n2));
+                }
+            default -> {
+            }
+            
+            
+            
+        // TODO code application logic here
+    }
+    
+}
